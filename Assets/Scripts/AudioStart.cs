69c0a38572e5865e2d662754192b9863bdd2454c
BrendanCranfield <61271276+BrendanCranfield@users.noreply.github.com>
2020-09-04 04:13:51 +0800

    3am Changes and Updates

---
+++ Assets/Scripts/AudioStart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioStart : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Entered Trigger");
+        audioSource.Play();
+    }
+}
